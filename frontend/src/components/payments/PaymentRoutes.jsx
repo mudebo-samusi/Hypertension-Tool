10c788f4a2fe9f21de7faf1b724235026c0a28ee
Arison99 <harrisondaviinci@gmail.com>
2025-05-16 05:13:01 +0300

    Update PaymentRoutes.jsx

--- frontend/src/components/payments/PaymentRoutes.jsx
+++ frontend/src/components/payments/PaymentRoutes.jsx
@@ -1,13 +1,19 @@
 // PaymentRoutes.jsx
 import { Routes, Route } from 'react-router-dom';
 import { PaymentDashboard } from './PaymentDashboard';
+import { PaymentList } from './PaymentList';
+import { PaymentAnalytics } from './PaymentAnalytics';
+import { PaymentForm } from './PaymentForm';
 import { PaymentProvider } from './PaymentContext';
 
 export const PaymentRoutes = () => {
   return (
     <PaymentProvider>
       <Routes>
-        <Route path="/payments" element={<PaymentDashboard />} />
+        <Route path="/" element={<PaymentDashboard />} />
+        <Route path="/list" element={<PaymentList />} />
+        <Route path="/analytics" element={<PaymentAnalytics />} />
+        <Route path="/new" element={<PaymentForm />} />
       </Routes>
     </PaymentProvider>
   );
