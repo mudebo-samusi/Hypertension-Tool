bbf0bbd60782692a606b1d99bd8caa7148ceb8f3
Arison99 <harrisondaviinci@gmail.com>
2025-05-17 18:01:55 +0300

    Update like.py

--- backend/models/like.py
+++ backend/models/like.py
@@ -5,9 +5,9 @@ from db import db
 class Like(db.Model):
     __tablename__ = 'likes'
     id           = db.Column(db.Integer, primary_key=True)
-    user_id      = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, index=True)
-    post_id      = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True, index=True)
-    comment_id   = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=True, index=True)
+    user_id      = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id      = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True)
+    comment_id   = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=True)
     created_at   = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     user         = db.relationship('User', back_populates='likes')
@@ -15,6 +15,8 @@ class Like(db.Model):
     comment      = db.relationship('Comment', back_populates='likes')
 
     __table_args__ = (
-        db.UniqueConstraint('user_id', 'post_id', name='uq_user_post_like'),
-        db.UniqueConstraint('user_id', 'comment_id', name='uq_user_comment_like'),
+        db.UniqueConstraint('user_id', 'post_id', name='unique_post_like'),
+        db.UniqueConstraint('user_id', 'comment_id', name='unique_comment_like'),
+        db.CheckConstraint('NOT(post_id IS NULL AND comment_id IS NULL)'),
+        db.CheckConstraint('NOT(post_id IS NOT NULL AND comment_id IS NOT NULL)')
     )
